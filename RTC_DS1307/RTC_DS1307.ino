6df01ac358b16dd342e45d8105517302c2a4dabf
TeesGuy <thomas.blumstein@etu.univ-tours.fr>
2020-12-11 18:05:24 +0100

    Update RTC_DS1307.ino

--- RTC_DS1307/RTC_DS1307.ino
+++ RTC_DS1307/RTC_DS1307.ino
@@ -1,15 +1,12 @@
 #include "RTC_DS1307.h"
 #include <Wire.h>
 
-const char* jour_semaine[7] = {"Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"};
-const char* nom_mois[12] = {"Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Décembre"};
+const char* joursemaine[7] = {"Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"};
+const char* nommois[12] = {"Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Décembre"};
 
+DS1307 clock;
 
-Date_RTC date_horloge;
-Heure_RTC heure_horloge;
-Horloge_RTC horloge;
-
-uint8_t decToBcd(uint8_t val){
+/*uint8_t decToBcd(uint8_t val){
     return ((val / 10 * 16) + (val % 10));
 }
 
@@ -94,28 +91,30 @@ void printTime() {
     Serial.print(" ");
     Serial.print(2000 + date_horloge.annee);
     Serial.println(" ");
-}
+}*/
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
     Wire.begin();
-    heure_horloge.seconde  = 1;
+    /*heure_horloge.seconde  = 1;
     heure_horloge.minute   = 1;
     heure_horloge.heure    = 1;
     date_horloge.jour_semaine   = 2;
     date_horloge.jour           = 10;
     date_horloge.nbr_mois      = 12;
-    date_horloge.annee          = 20;
-    printTime();
-    setDate();
+    date_horloge.annee          = 20;*/
+    clock.remplissage_heure(3,1,2);
+    clock.remplissage_date(3,11,12,20);
+    clock.printTime();
+    clock.setDate();
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  getDate();
-  printTime();
+  clock.getDate();
+  clock.printTime();
   delay(1000);
 
 }
