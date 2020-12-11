6e9fd5edacafbf9cd6a03dd032e435f9245946a5
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2020-12-11 18:37:04 +0100

    Update GPS.ino

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,20 +1,37 @@
-char* buffer_gps="";
+unsigned char buffer[64];                   // buffer array for data receive over serial port
+int count=0;
 
-char* GetGPS_Msg(){
-  buffer_gps=Serial1.read();
-  return buffer_gps;
-}
-
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial1.begin(9600);
+void setup()
+{ 
   Serial.begin(9600);
-  Serial1.print("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
+  Serial1.begin(9600);
+  //Serial1.write("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
 }
 
+void clearBufferArray()                     // function to clear buffer array
+{
+    for (int i=0; i<count;i++)
+    {
+        buffer[i]=NULL;
+    }                      // clear all index of array with command NULL
+}
+void GetGPS_msg()
+{
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(GetGPS_Msg());
+  while(Serial1.available())
+  {
+    buffer[count++]=Serial1.read();      // writing data into array
+    if(count == 64)break;
+  }
+  Serial.write(buffer,count);                 // if no data transmission ends, write buffer to hardware serial port
+  clearBufferArray();                         // call clearBufferArray function to clear the stored data from the array
+  count = 0; 
+    
+  //début : $ et fin : *
+  //temps d'aquisition limité
+}
+
+void loop() 
+{ 
+  GetGPS_msg();
 }
