f9e85eee0d1d14e7a07e96ce422e09cc5ef27e75
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2020-12-17 10:04:26 +0100

    Update GPS.ino

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -5,7 +5,6 @@ void setup()
 { 
   Serial.begin(9600);
   Serial1.begin(9600);
-  //Serial1.write("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
 }
 
 void clearBufferArray()                     // function to clear buffer array
@@ -13,25 +12,44 @@ void clearBufferArray()                     // function to clear buffer array
     for (int i=0; i<count;i++)
     {
         buffer[i]=NULL;
-    }                      // clear all index of array with command NULL
+    }
 }
+
 void GetGPS_msg()
 {
-
+  clearBufferArray();                         // call clearBufferArray function to clear the stored data from the array
+  count = 0;  
   while(Serial1.available())
   {
     buffer[count++]=Serial1.read();      // writing data into array
     if(count == 64)break;
   }
-  Serial.write(buffer,count);                 // if no data transmission ends, write buffer to hardware serial port
-  clearBufferArray();                         // call clearBufferArray function to clear the stored data from the array
-  count = 0; 
-    
-  //début : $ et fin : *
-  //temps d'aquisition limité
+    //Serial.write(buffer,count);     // if no data transmission ends, write buffer to hardware serial port
+
+}
+
+void Parser(unsigned char buffer[64])
+{
+  char** message;
+  char* mot;
+  for(int i=0; buffer[i]!='*'; i++)
+  {
+    if (buffer[i]!=',')
+    {
+      **message += buffer[i];
+    }
+    else 
+    {
+      *mot = **message;
+      Serial.println(*mot);
+      message++ ;
+    }
+  }
 }
 
 void loop() 
 { 
   GetGPS_msg();
+  Serial.write(buffer,count);     // if no data transmission ends, write buffer to hardware serial port
+  Parser(buffer);
 }
