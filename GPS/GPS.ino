f5dbb0bb36d6b442efbc7a01cac2ff2caa337859
TeesGuy <thomas.blumstein@etu.univ-tours.fr>
2020-12-21 15:41:32 +0100

    Update GPS.ino

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -5,7 +5,6 @@ void setup()
 { 
   Serial.begin(9600);
   Serial1.begin(9600);
-  Serial1.print("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
 }
 
 void clearBufferArray()                     // function to clear buffer array
@@ -14,7 +13,6 @@ void clearBufferArray()                     // function to clear buffer array
     {
         buffer[i]=NULL;
     }
-    int count=0;
 }
 
 void GetGPS_msg()
@@ -24,17 +22,12 @@ void GetGPS_msg()
   while(Serial1.available())
   {
     buffer[count++]=Serial1.read();      // writing data into array
-<<<<<<< HEAD
     if(count == 100)  clearBufferArray();
-=======
-    if(count == 100)break;
->>>>>>> a4caa598f8a1c38c46d326fdfef4885a616dc513
   }
     //Serial.write(buffer,count);     // if no data transmission ends, write buffer to hardware serial port
 
 }
 
-<<<<<<< HEAD
 bool Test_Synchro_GPS(char* result[]){
   if (result[0]=="$GPRMC"){
     if(result[2]=='A')return true;
@@ -51,36 +44,28 @@ void Choix_Msg_NMEA(char nmea){
   else Serial1.print("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
 }
 
-=======
->>>>>>> a4caa598f8a1c38c46d326fdfef4885a616dc513
 void Parser(unsigned char buffer[100])
 {
-  char *ptr;
-  char *result[20];   // Large enough to hold each datum
-  int count;
-  int i = 0;
-  ptr = strtok(buffer,",");   // Look for commas...
-  while (ptr != NULL)
+  char** message;
+  char* mot;
+  for(int i=0; buffer[i]!='*'; i++)
   {
-    result[i++] = ptr;
-    ptr = strtok (NULL, ",");
-  }
-  count = i;
-  for (i = 0; i < count; i++)
-  {
-    Serial.println(result[i]);   //$GPRMC,100245.000,A,4723.0089,N,00041.3693,E,0.00,7.04,171220,,,A*61
-    //Serial.print("     ");
+    if (buffer[i]!=',')
+    {
+      **message += buffer[i];
+    }
+    else 
+    {
+      *mot = **message;
+      Serial.println(*mot);
+      message++ ;
+    }
   }
 }
 
 void loop() 
 { 
   GetGPS_msg();
-<<<<<<< HEAD
   Serial.write(buffer,count);     // if no data transmission ends, write buffer to hardware serial port
-  //Parser(buffer);
-=======
-  //Serial.write(buffer,count);     // if no data transmission ends, write buffer to hardware serial port
   Parser(buffer);
->>>>>>> a4caa598f8a1c38c46d326fdfef4885a616dc513
 }
