365712f25811bad3079c1cf4f7c89abb92eaf07d
TeesGuy <thomas.blumstein@etu.univ-tours.fr>
2020-12-24 11:04:22 +0100

    Update GPS.ino

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,71 +1,89 @@
-unsigned char buffer[100];                   // buffer array for data receive over serial port
-int count=0;
-
-void setup()
-{ 
-  Serial.begin(9600);
-  Serial1.begin(9600);
-}
-
-void clearBufferArray()                     // function to clear buffer array
+unsigned char buffer[100];                   // bufferfer array for data receive over serial port
+int count = 0;
+char test[100] = "$GPRMC,100245.000,A,4723.0089,N,00041.3693,E,0.00,7.04,171220,,,A*61";
+void clearBufferArray()
 {
-    for (int i=100; i<count;i--)
-    {
-        buffer[i]=NULL;
-    }
+  for (int i = 0; i < count; i++)
+  {
+    buffer[i] = NULL;
+  }
 }
 
 void GetGPS_msg()
 {
-                         // call clearBufferArray function to clear the stored data from the array
-  count = 0;  
-  while(Serial1.available())
+
+  if (Serial1.available())
   {
-    buffer[count++]=Serial1.read();      // writing data into array
-    if(count == 100)  clearBufferArray();
+    while (Serial1.available())
+    {
+      buffer[count++] = Serial1.read();
+      if (count == 64)break;
+    }
+    //Serial.write(buffer,count);
+    //Parser(buffer);
+    //clearBufferArray();
+    count = 0;
   }
-    //Serial.write(buffer,count);     // if no data transmission ends, write buffer to hardware serial port
+}
+char *ptr;
+//char result[64];
+//int nbr;
+
+void Parser(char buf[100]) {
+  char newtest[100];
+  strcpy(newtest,buf);
+  Serial.println(newtest);
+  delay(10);
+
+//  nbr = 0;
+  ptr = strtok (newtest, ",");
+  //for (int i=0;i<14;i++){
+  while (ptr != NULL) {
+    //result[nbr] = ptr;
+    Serial.println(ptr);
+    ptr = strtok(NULL, ",");
+    
+//    nbr++;
+//    for (int i = 0; i < 15; i++) {
+//      Serial.println(result[i]);
+//      delay(10);
+//    }
+  }
 
 }
 
-bool Test_Synchro_GPS(char* result[]){
-  if (result[0]=="$GPRMC"){
-    if(result[2]=='A')return true;
+bool buffer_Synchro_GPS(char* result[]) {
+  if (result[0] == "$GPRMC") {
+    if (result[2] == 'A')return true;
     else return false;
   }
-  if (result[0]=="$GPGGA"){
-    if(result[6]=='0')return false;
+  if (result[0] == "$GPGGA") {
+    if (result[6] == '0')return false;
     else return true;
-}
-
-}
-void Choix_Msg_NMEA(char nmea){
-  if (nmea=="$GPGGA") Serial1.print("$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
-  else Serial1.print("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
-}
-
-void Parser(unsigned char buffer[100])
-{
-  char** message;
-  char* mot;
-  for(int i=0; buffer[i]!='*'; i++)
-  {
-    if (buffer[i]!=',')
-    {
-      **message += buffer[i];
-    }
-    else 
-    {
-      *mot = **message;
-      Serial.println(*mot);
-      message++ ;
-    }
   }
+
+}
+void Choix_Msg_NMEA(int nmea) {
+  if (nmea == 1) Serial1.println("$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
+  else  if (nmea == 2) Serial1.println("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
 }
 
-void loop() 
-{ 
+
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Choix_Msg_NMEA(2);
+}
+
+
+
+void loop()
+{
   GetGPS_msg();
-  Serial.write(buffer,count);     // if no data transmission ends, write buffer to hardware serial port
+  //Serial.println(buffer);     // if no data transmission ends, write bufferfer to hardware serial port
   Parser(buffer);
 }
