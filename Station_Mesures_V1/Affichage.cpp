b909076dbbad788f7e1fd11fa1a541cc1d0b4509
TeesGuy <thomas.blumstein@etu.univ-tours.fr>
2021-01-31 19:41:17 +0100

    it timer v1

---
+++ Station_Mesures_V1/Affichage.cpp
@@ -0,0 +1,241 @@
+#include "Affichage.h"
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+//Adafruit_GFX(319, 479);
+
+void Affichage::TFT_setup(void)
+{
+  tft.reset();
+  uint16_t identifier = tft.readID();
+  tft.begin(identifier);
+  tft.fillScreen(WHITE);
+  tft.setCursor(0, 0);
+  tft.setTextColor(BLACK);
+  tft.setTextSize(2);
+  tft.println("It's alive !");
+}
+
+void Affichage::TFT_fillScreen(uint16_t color)
+{
+  tft.fillScreen(color);
+}
+
+void Affichage::TFT_setCursor(int a, int b)
+{
+  tft.setCursor(a, b);
+}
+
+void Affichage::TFT_println(String text)
+{
+  tft.println(text);
+}
+
+void Affichage::TFT_affichage(	Calendrier::date_RTC curDate, Calendrier::date_RTC prevDate,
+                                Calendrier::heure_RTC curHour, Calendrier::heure_RTC prevHour,
+                                int curIndic, int prevIndic,
+                                char* curCity, char* prevCity,
+                                bool syncState,
+                                String curTemp, String prevTemp,
+                                String curPres, String prevPres,
+                                String curHum, String prevHum,
+                                String curQual, String prevQual,
+                                String curAcc, String prevAcc		)
+{
+  TFT_Affiche_Date(curDate, prevDate);
+  TFT_Affiche_Heure(curHour, prevHour);
+  TFT_Affiche_Indicateur_Ete_Hiver(curIndic, prevIndic);
+  TFT_Affiche_ville_ref_fuseau_horaire(curCity, prevCity);
+  TFT_Affiche_Etat_Synchro_GPS(syncState);
+  TFT_Affiche_Temperature(curTemp, prevTemp);
+  TFT_Affiche_Pression(curPres, prevPres);
+  TFT_Affiche_Humidite(curHum, prevHum);
+  TFT_Affiche_Qualite_Air(curQual, prevQual);
+  TFT_Affiche_Precision_Qualite_Air(curAcc, prevAcc);
+}
+
+void Affichage::TFT_Affiche_Date(Calendrier::date_RTC curDate, Calendrier::date_RTC prevDate)
+{
+  if (curDate.jour != prevDate.jour)
+  {
+    tft.setTextColor(BLACK);
+    tft.print("Date : ");
+    int x = tft.getCursorX();
+    int y = tft.getCursorY();
+    tft.setTextColor(WHITE);
+    tft.print(prevDate.jour);
+    tft.setCursor(x, y);
+    tft.setTextColor(BLACK);
+    tft.println(curDate.jour);
+  }
+  else {
+    tft.println(" ");
+  }
+}
+
+void Affichage::TFT_Affiche_Heure(Calendrier::heure_RTC curHour, Calendrier::heure_RTC prevHour)
+{
+  if (curHour.seconde != prevHour.seconde)
+  {
+    int x = tft.getCursorX();
+    int y = tft.getCursorY();
+    tft.setTextColor(WHITE);
+    tft.print(prevHour.heure);
+    tft.setCursor(x, y);
+    tft.setTextColor(BLACK);
+    tft.println(curHour.heure);
+  }
+  else {
+    tft.println(" ");
+  }
+}
+
+void Affichage::TFT_Affiche_Indicateur_Ete_Hiver(int curIndic, int prevIndic)
+{
+  if (curIndic != prevIndic)
+  {
+    String saison;
+    if (curIndic == 1) {
+      saison = "Été";
+    }
+    else saison ="Hiver";
+    
+    tft.setTextColor(BLACK);
+    tft.print("Heure d'");
+    int x = tft.getCursorX();
+    int y = tft.getCursorY();
+    tft.setTextColor(WHITE);
+    tft.print(saison);
+    tft.setCursor(x, y);
+    tft.setTextColor(BLACK);
+    tft.println(saison);
+
+  }
+  else {
+    tft.println(" ");
+  }
+}
+
+void Affichage::TFT_Affiche_ville_ref_fuseau_horaire(char* curCity, char* prevCity)
+{
+  if (*curCity != *prevCity)
+  {
+    tft.setTextColor(BLACK);
+    tft.print("Heure de ");
+    int x = tft.getCursorX();
+    int y = tft.getCursorY();
+    tft.setTextColor(WHITE);
+    tft.print(*prevCity);
+    tft.setCursor(x, y);
+    tft.setTextColor(BLACK);
+    tft.println(*curCity);
+  }
+  else {
+    tft.println(" ");
+  }
+}
+
+void Affichage::TFT_Affiche_Etat_Synchro_GPS(bool syncState)
+{
+  if (syncState)
+  {
+    tft.fillCircle(309, 10, 5, GREEN);
+  }
+  else {
+    tft.fillCircle(309, 10, 5, RED);
+  }
+}
+
+void Affichage::TFT_Affiche_Temperature(String curTemp, String prevTemp)
+{
+  if (curTemp != prevTemp)
+  {
+    tft.setTextColor(BLACK);
+    tft.print("Temperature : ");
+    int x = tft.getCursorX();
+    int y = tft.getCursorY();
+    tft.setTextColor(WHITE);
+    tft.print(prevTemp);
+    tft.setCursor(x, y);
+    tft.setTextColor(BLACK);
+    tft.println(curTemp);
+  }
+  else {
+    tft.println(" ");
+  }
+}
+
+void Affichage::TFT_Affiche_Pression(String curPres, String prevPres)
+{
+  if (curPres != prevPres)
+  {
+    tft.setTextColor(BLACK);
+    tft.print("Pression : ");
+    int x = tft.getCursorX();
+    int y = tft.getCursorY();
+    tft.setTextColor(WHITE);
+    tft.print(prevPres);
+    tft.setCursor(x, y);
+    tft.setTextColor(BLACK);
+    tft.println(curPres);
+  }
+  else {
+    tft.println(" ");
+  }
+}
+
+void Affichage::TFT_Affiche_Humidite(String curHum, String prevHum)
+{
+  if (curHum != prevHum)
+  {
+    tft.setTextColor(BLACK);
+    tft.print("Humidite : ");
+    int x = tft.getCursorX();
+    int y = tft.getCursorY();
+    tft.setTextColor(WHITE);
+    tft.print(prevHum);
+    tft.setCursor(x, y);
+    tft.setTextColor(BLACK);
+    tft.println(curHum);
+  }
+  else {
+    tft.println(" ");
+  }
+}
+
+void Affichage::TFT_Affiche_Qualite_Air(String curQual, String prevQual)
+{
+  if (curQual != prevQual)
+  {
+    tft.setTextColor(BLACK);
+    tft.print("Qualite air : ");
+    int x = tft.getCursorX();
+    int y = tft.getCursorY();
+    tft.setTextColor(WHITE);
+    tft.print(prevQual);
+    tft.setCursor(x, y);
+    tft.setTextColor(BLACK);
+    tft.println(curQual);
+  }
+  else {
+    tft.println(" ");
+  }
+}
+
+void Affichage::TFT_Affiche_Precision_Qualite_Air(String curAcc, String prevAcc)
+{
+  if (curAcc != prevAcc)
+  {
+    tft.setTextColor(BLACK);
+    tft.print("Precision qualite air : ");
+    int x = tft.getCursorX();
+    int y = tft.getCursorY();
+    tft.setTextColor(WHITE);
+    tft.print(prevAcc);
+    tft.setCursor(x, y);
+    tft.setTextColor(BLACK);
+    tft.println(curAcc);
+  }
+  else {
+    tft.println(" ");
+  }
+}
---
+++ Station_Mesures_V1/Station_Mesures_V1.ino
@@ -0,0 +1,178 @@
+//Page 53 du PDF de projet
+//#ifndef Affichage.h
+#include "Affichage.h"
+#define BASE_TEMPS_TIMER1_1S 49911U
+#define BASE_TEMPS_TIMER1_05S 57723U
+#define T_RH 1 // Période de gestion de l'événémént 1
+#define T_MAJH 40000 //4H=28800s, 6H=43200s donc environ 5h
+#define T_SyncGPS 3
+#define TCNT3_TIMER3 61630U // Période entre 2 IT Timer3 sur Overflow registre de comptage (environ 0.25s)
+//void checksensorStatus(void);
+
+
+volatile int T_Time_Out_Recuperation_Heure = 0;
+unsigned int T_Time_Out_MAJ_Heure = 3;
+volatile int nb_SyncGPS = 3;
+
+Affichage ecran;
+Bsec sensor;
+Calendrier Cal;
+GPS donnees_GPS;
+DS1307 clock;
+
+
+ISR (TIMER1_OVF_vect) //FCT INTERRUPTION
+{
+  noInterrupts();
+  /* PUSH Rn
+    LDS Rn, SREG
+    PUSH Rn*/
+  //TRAITEMENT LED
+  T_Time_Out_Recuperation_Heure--;
+  T_Time_Out_MAJ_Heure--;
+  //FIN TRAITEMENT LED
+  /*POP Rn
+    STS SREG, Rn
+    POP Rn*/
+  interrupts();
+}
+
+
+
+void setup()
+{
+
+  ecran.TFT_setup();
+  Serial.begin(9600);
+  Wire.begin();
+  sensor.begin(BME680_I2C_ADDR_PRIMARY, Wire);
+  //checksensorStatus();
+
+  bsec_virtual_sensor_t sensorList[10] = {
+    BSEC_OUTPUT_RAW_TEMPERATURE,
+    BSEC_OUTPUT_RAW_PRESSURE,
+    BSEC_OUTPUT_RAW_HUMIDITY,
+    BSEC_OUTPUT_RAW_GAS,
+    BSEC_OUTPUT_IAQ,
+    BSEC_OUTPUT_STATIC_IAQ,
+    BSEC_OUTPUT_CO2_EQUIVALENT,
+    BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+  };
+
+  sensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
+
+  donnees_GPS.Parser(donnees_GPS.buffer);
+  Cal.Extract_date_heure_from_GPS(donnees_GPS.results[1], donnees_GPS.results[9]);
+  Cal.initialiser_fuseaux();
+  Cal.Correction_Heure_Date(Cal.UTC, Cal.f1, 0);
+  clock.setDate(Cal.UTC);
+  Cal.UTC = clock.getDate(Cal.UTC);
+  Cal.locale = Cal.Correction_Heure_Date(Cal.UTC, Cal.f1, Cal.indicateur_ete_hiver);
+  clock.setDate(Cal.locale);
+
+  if (sensor.run()) {
+    ecran.prevTemp = sensor.rawTemperature;
+    ecran.prevPres = sensor.pressure;
+    ecran.prevHum = sensor.humidity;
+    ecran.prevQual = sensor.iaq;
+    ecran.prevAcc = sensor.iaqAccuracy;
+  }
+
+  ecran.prevDate = Cal.locale.date;
+  ecran.prevHour = Cal.locale.heure;
+  ecran.prevIndic = 3;
+  ecran.prevCity = Cal.f1.villes;
+  ecran.syncState = donnees_GPS.buffer_Synchro_GPS(donnees_GPS.Parser(donnees_GPS.GetGPS_msg()));
+
+  ecran.TFT_affichage(   ecran.curDate, ecran.prevDate,
+                         ecran.curHour, ecran.prevHour,
+                         ecran.curIndic , ecran.prevIndic,
+                         ecran.curCity , ecran.prevCity,
+                         ecran.syncState,
+                         ecran.curTemp, ecran.prevTemp,
+                         ecran.curPres, ecran.prevPres,
+                         ecran.curHum, ecran.prevHum,
+                         ecran.curQual, ecran.prevQual,
+                         ecran.curAcc, ecran.prevAcc   );
+  //delay(5000);
+
+}
+
+void loop()
+{
+  if (T_Time_Out_Recuperation_Heure <= 0)
+  {
+    if (sensor.run()) {
+      ecran.curTemp = sensor.rawTemperature;
+      ecran.curPres = sensor.pressure;
+      ecran.curHum = sensor.humidity;
+      ecran.curQual = sensor.iaq;
+      ecran.curAcc = sensor.iaqAccuracy;
+    }
+
+    ecran.curDate = Cal.locale.date;
+    ecran.curHour = Cal.locale.heure;
+    ecran.curIndic = Cal.indicateur_ete_hiver;
+    ecran.curCity = Cal.f1.villes;
+    ecran.syncState = donnees_GPS.buffer_Synchro_GPS(donnees_GPS.Parser(donnees_GPS.GetGPS_msg()));
+
+    //ecran.TFT_fillScreen(WHITE);
+    ecran.TFT_setCursor(0, 0);
+    ecran.TFT_println(" ");
+    ecran.TFT_affichage(   ecran.curDate, ecran.prevDate,
+                           ecran.curHour, ecran.prevHour,
+                           ecran.curIndic , ecran.prevIndic,
+                           ecran.curCity , ecran.prevCity,
+                           ecran.syncState,
+                           ecran.curTemp, ecran.prevTemp,
+                           ecran.curPres, ecran.prevPres,
+                           ecran.curHum, ecran.prevHum,
+                           ecran.curQual, ecran.prevQual,
+                           ecran.curAcc, ecran.prevAcc   );
+    ecran.prevTemp = ecran.curTemp;
+    ecran.prevPres = ecran.curPres;
+    ecran.prevHum = ecran.curHum;
+    ecran.prevQual = ecran.curQual;
+    ecran.prevAcc = ecran.curAcc;
+    *ecran.prevCity = *ecran.curCity;
+    ecran.prevDate = ecran.curDate;
+    ecran.prevHour = ecran.curHour;
+    ecran.prevIndic = ecran.curIndic;
+  }
+
+  if (T_Time_Out_MAJ_Heure <= 0 && nb_SyncGPS >= 0)
+  { bool test = donnees_GPS.buffer_Synchro_GPS(donnees_GPS.gprmc);
+
+    if (test == false) {
+      Serial.println("Message GPS non valide");
+      donnees_GPS.GetGPS_msg();
+      donnees_GPS.Parser(donnees_GPS.buffer);
+      T_Time_Out_MAJ_Heure = T_SyncGPS;
+    }
+    if (test == true) {
+      Serial.println("Message GPS valide!");
+      if (donnees_GPS.gprmc.date == 0) {
+        Cal.UTC = Cal.Extract_date_heure_from_GPS(donnees_GPS.gprmc.heure, donnees_GPS.gprmc.longitude);//pour regler le soucis du parser qui "efface" les indicateurs nord et sud
+      }
+      else {
+        Cal.UTC = Cal.Extract_date_heure_from_GPS(donnees_GPS.gprmc.heure, donnees_GPS.gprmc.date);
+      }
+      clock.setDate(Cal.UTC);
+      Cal.locale = Cal.Correction_Heure_Date(Cal.UTC, Cal.f16, Cal.indicateur_ete_hiver);
+      clock.setDate(Cal.locale);
+      if (nb_SyncGPS > 0) {
+        T_Time_Out_MAJ_Heure = T_SyncGPS;
+        nb_SyncGPS--;
+      }
+      else {
+        T_Time_Out_MAJ_Heure = T_MAJH;
+        nb_SyncGPS = 1;
+      }
+    }
+  }
+
+
+  //delay(5000);
+}
